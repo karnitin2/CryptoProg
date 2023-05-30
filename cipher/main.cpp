fc26390b514a556c0eeaed8da9a7ddd65267de37
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-05-30 19:46:12 +0300

    Update main.cpp

--- cipher/main.cpp
+++ cipher/main.cpp
@@ -8,16 +8,13 @@
 #include <cryptopp/files.h>
 #include <cryptopp/modes.h>
 #include <cryptopp/aes.h>
-
 using namespace CryptoPP;
 using namespace std;
-
 int main()
 {
     int mode;
     cout << "Введите режим работы (0-зашифрование, 1-расшифрование): ";
     cin >> mode;
-
     string inputFilename, outputFilename, password;
     cout << "Введите имя файла с исходными данными: ";
     cin >> inputFilename;
@@ -25,13 +22,10 @@ int main()
     cin >> outputFilename;
     cout << "Введите пароль: ";
     cin >> password;
-
     byte key[SHA256::DIGESTSIZE];
     SHA256().CalculateDigest(key, (byte*)password.c_str(), password.length());
-
     byte iv[AES::BLOCKSIZE];
     memset(iv, 0x00, AES::BLOCKSIZE);
-
     if (mode == 0) {
         cout << "Зашифрование файла " << inputFilename << " в файл " << outputFilename << endl;
         CBC_Mode<AES>::Encryption encryptor(key, sizeof(key), iv);
@@ -41,6 +35,5 @@ int main()
         CBC_Mode<AES>::Decryption decryptor(key, sizeof(key), iv);
         FileSource(inputFilename.c_str(), true, new StreamTransformationFilter(decryptor, new FileSink(outputFilename.c_str())));
     }
-
     return 0;
 }
