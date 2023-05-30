2eae6157def3006bd2d9292dd801dbbdf6b0460d
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-05-30 19:44:44 +0300

    Update main.cpp

--- hash/main.cpp
+++ hash/main.cpp
@@ -6,18 +6,31 @@
 #include <iostream>
 using namespace CryptoPP;
 using namespace std;
+
 int main()
 {
-    string hashMsg, msg;
-    FileSource("/home/stud/4/4", true, new StringSink(msg));
+    string gsm, msg, result, result1;
+    FileSource("/home/stud/C++Projects/4pract/hash/text_1.txt", true, new StringSink(msg));
     msg.resize(msg.size() - 1);
     cout << "Text from file: " << msg << endl;
-    HexEncoder encoder(new FileSink(cout));
+    HexEncoder encoder(new StringSink(result));
     Weak::MD5 hash;
     hash.Update((const byte*)&msg[0], msg.size());
-    hashMsg.resize(hash.DigestSize());
-    hash.Final((byte*)&hashMsg[0]);
-    cout << "Text HASH: ";
-    StringSource(hashMsg, true, new Redirector(encoder));
-    cout <<"\n";
+    gsm.resize(hash.DigestSize());
+    hash.Final((byte*)&gsm[0]);
+    cout << "HASH: ";
+    StringSource(gsm, true, new Redirector(encoder));
+    cout << result << "\n\n";
+    // Блок для проверки работоспособности кода выше:
+    HexEncoder encoder1(new StringSink(result1));
+    string gsm1, msg1 = "russian forward";
+    cout << "Text: " << msg1 << endl;
+    hash.Update((const byte*)&msg1[0], msg1.size());
+    gsm1.resize(hash.DigestSize());
+    hash.Final((byte*)&gsm1[0]);
+    cout << "HASH: ";
+    StringSource(gsm1, true, new Redirector(encoder1));
+    cout << result1 << endl;
+    if (result == result1)
+        cout << "\nAll right\n";
 }
